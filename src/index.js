270d5a0c1a33ea2ec1f536cec6d3d1fadbc02ce1
Kunal Thakur <kunalt258@gmail.com>
2025-12-24 22:29:36 +0530

    Fix API URLs for production

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
+export const API_BASE_URL =
+  process.env.REACT_APP_API_URL || "http://localhost:5000";
+
 const clientId =
   "347843253080-m0j3fvrjvps2075v68063jrlpj95prv6.apps.googleusercontent.com";
 
